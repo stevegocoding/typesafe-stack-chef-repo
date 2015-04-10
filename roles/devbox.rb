9e66ec30d0888a8cd0417b0de7602d668e7ff1ef
stevegocoding <stevegocoding@gmail.com>
2015-04-10 00:27:53 -0400

    Added scala recipe

--- roles/devbox.rb
+++ roles/devbox.rb
@@ -7,10 +7,15 @@ override_attributes(
     "oracle" => { 
       "accept_oracle_download_terms" => true 
     }
+  },
+  "scala" => {
+    "version" => '2.11.6',
+    "url" => 'http://downloads.typesafe.com/scala/2.11.6/scala-2.11.6.tgz?_ga=1.146636148.1994524479.1426856063'
   }
 )
 
 run_list(
   "recipe[java::oracle]",
+  "recipe[scala]",
   "recipe[typesafe-stack]"
 )
